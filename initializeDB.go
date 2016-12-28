445ecee3f580eb0fd56e1fd37c1ea35468d048ac
Joey Cozza <joeycozza@gmail.com>
2016-12-27 21:43:28 -0700

    building the schema

--- initializeDB.go
+++ initializeDB.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-func initialize() {
+//Initialize is so good
+func Initialize() {
 	db, err := sql.Open("postgres", postgresPath)
 	if err != nil {
 		log.Fatal(err)
@@ -16,39 +18,60 @@ func initialize() {
 	err = db.Ping()
 	if err != nil {
 		log.Fatal("Ping error: ", err)
+	} else {
+		fmt.Println("Ping worked")
 	}
+	var count int
+	err = db.QueryRow(`SELECT COUNT(*) FROM pg_catalog.pg_tables where tableowner = 'cozza';`).Scan(&count)
 
-	var (
-		id   int
-		name string
-	)
-
-	rows, err := db.Query(`CREATE TABLE "public"."expense" (
-    "id" serial,
-    "item" text NOT NULL,
-    "cost" numeric NOT NULL,
-    "reimbursed" bool NOT NULL DEFAULT 'false',
-    "purchase_date" date NOT NULL DEFAULT now(),
-    PRIMARY KEY ("id")
-		);`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-
-		err := rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal("Row scanning error: ", err)
-		}
-		log.Println(id, name)
-
-	}
-
-	err = rows.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	if count > 0 {
+		fmt.Println("Not building tables because they already exist")
+		return
+	}
+	_, err = db.Exec(`CREATE TABLE public.brisket (
+													id serial,
+													price numeric,
+													weight_pre_trim numeric,
+													weight_post_trim numeric,
+													weight_post_cook numeric,
+													rub text,
+													sous_vide_time numeric,
+													sous_vide_temp int,
+													smoker_time numeric,
+													smoker_temp int,
+													final_temp int,
+													meta jsonb,
+													PRIMARY KEY (id)
+										);
+										
+									CREATE TABLE public.event (
+													id serial,
+													type text,
+													event_date date,
+													description jsonb,
+													estimated_mouths int,
+													PRIMARY KEY (id)
+									);
+										
+									CREATE TABLE "public"."expense" (
+													id serial,
+													item text NOT NULL,
+													cost numeric NOT NULL,
+													reimbursed bool NOT NULL DEFAULT 'false',
+													purchase_date date NOT NULL DEFAULT now(),
+													event_id integer REFERENCES event (id),
+													PRIMARY KEY ("id")
+									);
+									
+										`)
+
+	fmt.Println("finished exec")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
 }
